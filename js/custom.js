79dff3e1223f6e1e11ecc54ba401c74bee0186d1
anrbl <eujin0903@gmail.com>
2023-09-13 16:50:57 +0900

    jsfinal

--- js/custom.js
+++ js/custom.js
@@ -103,7 +103,7 @@ $(function () {
 var container = document.getElementById('map');
 var options = {
     center: new kakao.maps.LatLng(35.85600929345938, 128.62483279194302),
-    level: 3
+    level: 2
 };
 
 var map = new kakao.maps.Map(container, options);
